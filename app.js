2acd16036574245e9933224115f8317e5d778582
Andy Gu <43463033+zjuguyunzhe@users.noreply.github.com>
2020-02-29 07:07:28 +0800

    add practice block

--- app.js
+++ app.js
@@ -7,7 +7,19 @@
  * Step 2: Import from crypto-js module the sha265 library
  */
 
-var CryptoJS = require("crypto-js"); 
+var CryptoJS = require("crypto-js");
+const BlockClass = require('./block.js');
+
+/**
+ * Creating a block object
+ */
+const block = new BlockClass.Block("Test Block");
+
+// Generating the block hash
+block.generateHash().then((result) => {
+	console.log(`Block Hash: ${result.hash}`);
+	console.log(`Block: ${JSON.stringify(result)}`);
+}).catch((error) => {console.log(error)});
 
 /**
  * Variables: Do not change variable values to be able to answer the quiz
